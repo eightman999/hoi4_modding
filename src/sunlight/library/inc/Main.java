20594eeb75a0a46264b96dc519467d9265cb79b4
eightman999 <90958746+eightman999@users.noreply.github.com>
2022-10-09 17:27:45 +0900

    Update Main.java

--- src/sunlight/library/inc/Main.java
+++ src/sunlight/library/inc/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static String LSTR = "";
     public static  String MSTR = "";
     public static int L_mode = 0;
-    public static int mode = 0;
+    static int mode = 0;
     public static void main(String[] args) {
         ROOT root = new ROOT();
         root.start();

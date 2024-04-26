90e34d8049c6c1af9463951ffd33a2ec2885a59c
eightman <eightman124@gmail.com>
2024-04-26 09:35:18 +0900

    a

--- src/eightman/library/GUI/language.java
+++ src/eightman/library/GUI/language.java
@@ -1,75 +1,67 @@
 package eightman.library.GUI;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class language {
 
-  public static String DONE = "Done";
-  public static String LOCALIZE = "Localize";
-  public static String EQUIPMENT = "Equipment";
-  public static String COUNTRY = "country";
-  public static String NONE = "None";
-  public static String MODE_SELECT = "Select Mode";
-  public static String LG_SELECT = "Select language";
-  public static String ENGLISH = "English";
-  public static String JAPANESE = "Japanese";
-  public static String Please_Select_mode = "Please Select mode";
-  public static String GFX = "GFX";
-  public static String GOAL = "Goals";
-  public static String SHL = "Ship Hull";
-  public static String Title = "Hoi4 modding tool";
-  public static String File = "File";
-  public static String Save = "Save";
-  public static String combined = "combined";
-  public static String str_loc = "Starts the creation of a translation file.";
-  public static String LR_no = "";
-  public static String CCC =
-    "It is described by eightman.library and software created by eightman";
-  public static String IMAGE = "Image File";
+    public static String DONE = "Done";
+    public static String LOCALIZE = "Localize";
+    public static String EQUIPMENT = "Equipment";
+    public static String COUNTRY = "country";
+    public static String NONE = "None";
+    public static String MODE_SELECT = "Select Mode";
+    public static String LG_SELECT = "Select language";
+    public static String ENGLISH = "English";
+    public static String JAPANESE = "Japanese";
+    public static String Please_Select_mode = "Please Select mode";
+    public static String GFX = "GFX";
+    public static String GOAL = "Goals";
+    public static String SHL = "Ship Hull";
+    public static String Title = "Hoi4 modding tool";
+    public static String File = "File";
+    public static String Save = "Save";
+    public static String combined = "combined";
+    public static String str_loc = "Starts the creation of a translation file.";
+    public static String LR_no = "";
+    public static String CCC =
+            "It is described by eightman.library and software created by eightman";
+    public static String IMAGE = "Image File";
 
-  public static void C_languages() {
-    if (Main_GUI.L_mode == 0) {
-      DONE = "完了";
-      LOCALIZE = "翻訳ファイル作成";
-      EQUIPMENT = "装備作成";
-      COUNTRY = "国家作成";
-      NONE = "未選択";
-      MODE_SELECT = "モード選択";
-      LG_SELECT = "言語選択";
-      ENGLISH = "英語";
-      JAPANESE = "日本語";
-      Please_Select_mode = "モードを選択してください";
-      SHL = "船体設定";
-      GOAL = "NF用画像";
-      GFX = "画像適用";
-      Title = "Hoi4 mod開発支援ツール";
-      File = "ファイル";
-      Save = "セーブ";
-      combined = "結合";
-      str_loc = "翻訳ファイルの作成を開始します。";
-      LR_no = "左右で行数が違います";
-      IMAGE = "画像ファイル";
-      System.out.println(DONE);
-      // System.out.println(L_mode);
-    } else if (Main_GUI.L_mode == 1) {
-      DONE = "Done";
-      LOCALIZE = "Localize";
-      EQUIPMENT = "Equipment";
-      COUNTRY = "country";
-      NONE = "None";
-      MODE_SELECT = "Select Mode";
-      LG_SELECT = "Select language";
-      ENGLISH = "English";
-      JAPANESE = "Japanese";
-      Please_Select_mode = "Please Select mode";
-      GFX = "GFX";
-      GOAL = "Goals";
-      SHL = "Ship Hull";
-      Title = "Hoi4 modding tool";
-      File = "File";
-      Save = "Save";
-      combined = "combined";
-      str_loc = "Starts the creation of a translation file.";
-      LR_no = "The number of lines is different on the left and right.";
-      IMAGE = "Image File";
+    public static void C_languages() {
+        String languageFile = Main_GUI.L_mode == 0 ? "jp_ja.json" : "en_en.json";
+        JSONObject langJson = new JSONObject(readFile(languageFile));
+        DONE = langJson.getString("DONE");
+        LOCALIZE = langJson.getString("LOCALIZE");
+        EQUIPMENT = langJson.getString("EQUIPMENT");
+        COUNTRY = langJson.getString("country");
+        NONE = langJson.getString("None");
+        MODE_SELECT = langJson.getString("Select Mode");
+        LG_SELECT = langJson.getString("Select language");
+        ENGLISH = langJson.getString("English");
+        JAPANESE = langJson.getString("Japanese");
+        Please_Select_mode = langJson.getString("Please Select mode");
+        GFX = langJson.getString("GFX");
+        GOAL = langJson.getString("Goals");
+        SHL = langJson.getString("Ship Hull");
+        Title = langJson.getString("Hoi4 modding tool");
+        File = langJson.getString("File");
+        Save = langJson.getString("Save");
+        combined = langJson.getString("combined");
+        str_loc = langJson.getString("Starts the creation of a translation file.");
+        LR_no = langJson.getString("The number of lines is different on the left and right.");
+        IMAGE = langJson.getString("Image File");
+        private static String readFile (String filename){
+            String content = "";
+            try {
+                content = new String(Files.readAllBytes(Paths.get(filename)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content;
+        }
     }
-  }
 }

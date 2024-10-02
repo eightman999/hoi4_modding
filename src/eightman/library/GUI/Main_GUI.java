bb5abcd2174104fe1d0328c67433f5a6d1f42b22
eightman <eightman124@gmail.com>
2024-10-02 14:54:02 +0900

    Update Main_GUI.java

--- src/eightman/library/GUI/Main_GUI.java
+++ src/eightman/library/GUI/Main_GUI.java
@@ -26,8 +26,7 @@ import java.util.Date;
 import java.util.Objects;
 
 import static eightman.library.Core.*;
-import static eightman.library.GUI.System.Mac_OS.isMac;
-import static eightman.library.GUI.System.Mac_OS.mac_system;
+import static eightman.library.GUI.System.Mac_OS.*;
 import static eightman.library.GUI.language.*;
 import static eightman.library.GUI.language.C_languages;
 import static javax.swing.UIManager.setLookAndFeel;
